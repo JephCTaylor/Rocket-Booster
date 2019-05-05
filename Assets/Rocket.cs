6c83ad9ab02b6e3ab87eeab5a04e8d3836ea5153
JephCTaylor <jctaylor@mail.usf.edu>
2019-05-04 20:07:11 -0400

    movement updated

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
 	Rigidbody rigidBody;
 	AudioSource audioSource;
 
@@ -17,14 +20,17 @@ public class Rocket : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
-		ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-	private void ProcessInput()
-	{
+    private void Thrust()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+
 		if (Input.GetKey(KeyCode.Space))
 		{
-			rigidBody.AddRelativeForce(Vector3.up);
+			rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
 			if (!audioSource.isPlaying)
 			{
 				audioSource.Play();
@@ -34,14 +40,23 @@ public class Rocket : MonoBehaviour {
 		{
 			audioSource.Stop();
 		}
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
 		if (Input.GetKey(KeyCode.A))
 		{
-			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
-			transform.Rotate(-Vector3.forward);
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
 		}
-	}
+        
+        rigidBody.freezeRotation = false;
+    }
 }
